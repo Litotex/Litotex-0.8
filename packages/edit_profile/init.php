e03d08bc04a0bb55e76d8a7594a7f8358b8b933c
Jonas Schwabe <jonas.schwabe@gmail.com>
2010-09-15 22:16:58 +0200

    Fix user save bug when using buffers

--- packages/edit_profile/init.php
+++ packages/edit_profile/init.php
@@ -62,7 +62,7 @@ class package_edit_profile extends package {
 				exit();
 			}
 		 // BUG ?? geht nur wenn auf false steht	
-			package::$user->setData('email',$email,false);
+			package::$user->setData('email',$email, true);
 		}	
 		
 		if ($password !=''){
