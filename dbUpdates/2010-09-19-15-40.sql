5f996b36ec262c2226c08c243025f491908dc953
SnooP <info@freebg.de>
2010-09-19 15:40:41 +0200

    Update für MailModul

---
+++ dbUpdates/2010-09-19-15-40.sql
@@ -0,0 +1,3 @@
+INSERT INTO `lttx1_options` (`ID`, `package`, `key`, `value`, `default`) VALUES (27, 'mail', 'AdminEmail', 'info@freebg.de', 'info@freebg.de');
+INSERT INTO `lttx1_options` (`ID`, `package`, `key`, `value`, `default`) VALUES (28, 'mail', 'AdminEmailName', 'adminEmailName', 'Admin');
+
