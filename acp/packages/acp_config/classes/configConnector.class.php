abca7746fd216880a25addf6291b51abe1eb13e2
Jonas Schwabe <jonas.schwabe@gmail.com>
2010-10-21 17:14:19 +0200

    Argh :D I knew it...

--- acp/packages/acp_config/classes/configConnector.class.php
+++ acp/packages/acp_config/classes/configConnector.class.php
@@ -2,8 +2,8 @@
 require_once "configConnector.class.php";
 abstract class configConnector{
 	protected $_configElements = array();
-	public function __construct(); //Do some handshake... what ever...
-	public function __destruct(); //Save some changes... etc
+	public function __construct(){} //Do some handshake... what ever...
+	public function __destruct(){} //Save some changes... etc
 	abstract public function getData();
 	abstract public function saveData();
 	public final function registerConfig(config $config){
