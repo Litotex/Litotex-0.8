ef33ae3f5900962910373658deea4cd22466a260
Jonas Schwabe <jonas.schwabe@gmail.com>
2011-01-07 12:14:58 +0100

    SQL Updated

--- dbUpdates/2010-09-19-15-40.sql
+++
@@ -1,3 +0,0 @@
-INSERT INTO `lttx1_options` (`ID`, `package`, `key`, `value`, `default`) VALUES (27, 'mail', 'AdminEmail', 'info@freebg.de', 'info@freebg.de');
-INSERT INTO `lttx1_options` (`ID`, `package`, `key`, `value`, `default`) VALUES (28, 'mail', 'AdminEmailName', 'adminEmailName', 'Admin');
-
