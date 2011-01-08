614870f60790409782164ebef428c85dd67f6c17
Jonas Schwabe <jonas.schwabe@gmail.com>
2011-01-08 13:20:38 +0100

    INstallation process now works quite good (not ready! anyways)

--- packages/core/classes/installer.class.php
+++ packages/core/classes/installer.class.php
@@ -14,13 +14,13 @@ abstract class installer{
 	}
 	private final function _checkData(){
 		if(!is_dir($this->_location . '/template'))
-			throw new Exception("Template directory was not found in $this->_location/template");
+			throw new lttxFatalError("Template directory was not found in $this->_location/template");
 		if(!is_dir($this->_location . '/package'))
-			throw new Exception("Package directory was not found in $this->_location/package");
+			throw new lttxFatalError("Package directory was not found in $this->_location/package");
 		if(!file_exists($this->_location . '/package/init.php'))
-			throw new Exception("Package init file directory was not found in $this->_location/package/init.php");
+			throw new lttxFatalError("Package init file directory was not found in $this->_location/package/init.php");
 		if(file_exists($this->_location . '/database/install.sql') && !file_exists($this->_location . '/database/remove.sql')){
-			throw new Exception("Installation package contains an installation SQL File but lacks support for deinstall it. Please add remove.sql in order to install this application.");
+			throw new lttxFatalError("Installation package contains an installation SQL File but lacks support for deinstall it. Please add remove.sql in order to install this application.");
 		return true;
 		}
 	}
@@ -45,13 +45,12 @@ abstract class installer{
 		}
 	}
 	private final function _install(){
-//		var_dump($this->_getVersionNumber($this->_packageName, $this->_location . '/package/init.php'));
 		$this->_recursiveCopy($this->_location . '/template', TEMPLATE_DIRECTORY . $this->_packageName);
 		$this->_recursiveCopy($this->_location . '/package', MODULES_DIRECTORY . $this->_packageName);
 		$this->_patchDatabase(true);
 		$this->_freeInstall();
 	}
-	private final function _patchDatabase($install){
+	private final function _patchDatabase($install, $noThrow = false){
 		if($install){
 			if(!file_exists($this->_location . '/database/install.sql'))
 				return true;
@@ -65,35 +64,21 @@ abstract class installer{
 		if(!isset($data->query))
 			return true;
 		foreach($data->query as $query){
-//			var_dump($this->_compareVersion('0.8.1', $query->attributes()->version));
+			if(packages::compareVersionNumbers(packages::getVersionNumber($this->_packageName), $query->attributes()->version) == 1 || !$install){
+				package::$db->Execute($data->query);
+				$errorMsg = package::$db->ErrorMsg();
+				if($errorMsg){
+					try{
+						if($install)
+							$this->_patchDatabase(false, true);
+					} catch (Exception $e) {}
+					throw new lttxError('E_installerMySQLFailure', $errorMsg);
+				}
+			}
 		}
-	}
-	private final function _compareVersion($v1, $v2){
-		if(!preg_match('/^[0-9]{1,}?\.[0-9]{1,}?\.[0-9]{1,}$/', $v1))
-			return false;
-		if(!preg_match('/^[0-9]{1,}?\.[0-9]{1,}?\.[0-9]{1,}$/', $v2))
-			return false;
-		$v1 = explode('.', $v1);
-		$v2 = explode('.', $v2);
-		if($v1[0] > $v2[0]){
-			return 1;
-		} else if($v1[0] < $v2[0]){
-			return -1;
-		}
-		if($v1[1] > $v2[1]){
-			return 1;
-		} else if($v1[1] < $v2[1]){
-			return -1;
-		}
-		if($v1[2] > $v2[2]){
-			return 1;
-		} else if($v1[2] < $v2[2]){
-			return -1;
-		}
-		return 0;
 	}
 	private final function _getVersionNumber($modulName, $file){
-		return eval(preg_replace('/^<\?php/', '', str_replace('class package_'.$modulName, 'class package_' . $modulName . 'INSTALLER', file_get_contents($file))) . " \$vars = get_class_vars('package_".$modulName."INSTALLER'); return (isset(\$vars['version']))?\$vars['version']:false;");
+		return eval(preg_replace('/<\?php|\?>|/', '', str_replace('class package_'.$modulName, 'class package_' . $modulName . 'INSTALLER', file_get_contents($file))) . " \$vars = get_class_vars('package_".$modulName."INSTALLER'); return (isset(\$vars['version']))?\$vars['version']:false;");
 	}
 	protected abstract function _freeInstall();
 }
--- tpl/default/lang/de.lang.php
+++ tpl/default/lang/de.lang.php
@@ -6,3 +6,4 @@ E_fatalErrorOccured = "Es ist ein schwerer Fehler aufgetreten, bitte versuchen s
 E_noPermission = "Sie haben nicht genügend berechtigungen diese Seite zu betrachten."
 E_couldNotRetrievePackageList = "Es konnte keine aktuele Paketliste vom Litotex Update Server abgerufen werden. Möglicherweise ist der Server derzeit nicht erreichbar. Sollte der Fehler nach einiger Zeit immer noch auftreten wenden sie sich für hilfe bitte an das Forum unter <a href="http://www.freebg.de">Freebg.de</a>."
 E_wrongListRetrieved = "Die empfangene Datei enthält keine Paketliste."
+E_installerMySQLFailure = "Es ist ein MySQL Fehler beim installieren aufgetreten: '%s'"
