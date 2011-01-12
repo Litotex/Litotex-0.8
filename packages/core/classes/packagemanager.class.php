9d3231195819c9632fbd7ffb495405dc96737105
Jonas Schwabe <jonas.schwabe@gmail.com>
2011-01-12 21:49:40 +0100

    fix last commit

--- packages/core/classes/packagemanager.class.php
+++ packages/core/classes/packagemanager.class.php
@@ -624,7 +624,7 @@ class packages{
 			}
 			//Now we have to write all this to the database :)
 			package::$db->Execute("INSERT INTO  `lttx_packageList` (`ID`, `name`, `installed`, `update`, `critupdate`, `version`, `description`, `author`, `authorMail`, `signed`, `signedOld`, `fullSigned`, `fullSignedOld`, `releaseDate`, `signInfo`, `dependencies`, `changelog`)
-				VALUES (NULL ,  ?,  ?,  ?,  ?,  ?,  ?,  ?,  ?,  ?,  ?,  ?,  ?,  ?, ?, ?);",
+				VALUES (NULL ,  ?,  ?,  ?,  ?,  ?,  ?,  ?,  ?,  ?,  ?,  ?,  ?,  ?, ?, ?, ?);",
 				array($name, $installed, $update, $critupdate, $version, $description, $author, $authorMail, $signed, $signedOlder, $fullSigned, $fullSignedOlder, $releaseDate, serialize($signInfo), serialize($dependency), serialize($changelog)));
 		}
 	}
