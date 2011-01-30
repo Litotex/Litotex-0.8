e65e390973cd4b04b244edc698a654ec8b108c9c
Jonas Schwabe <jonas.schwabe@gmail.com>
2011-01-30 22:22:56 +0100

    Does not need cachefiles in HEAD

--- acp/files/cache/login.1.0.0.0.8.x.cache/database/remove.sql
+++
@@ -1,8 +0,0 @@
-<?xml version="1.0"?>
-<sql>
-<query version="0.1.0">DROP TABLE `lttx_test1`;</query>
-<query version="0.2.0">DROP TABLE `lttx_test2`;</query>
-<query version="0.3.0">DROP TABLE `lttx_test3`;</query>
-<query version="0.4.0">DROP TABLE `lttx_test4`;;</query>
-<query version="0.5.0">DROP TABLE `lttx_test5`;</query>
-</sql>
--- acp/files/cache/login.1.0.0.0.8.x.cache/installer.php
+++
@@ -1,4 +0,0 @@
-<?php
-class installer_login extends installer{
-	protected function _freeInstall(){}
-}
