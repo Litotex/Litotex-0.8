a1e563c4f84773de997eedb0a36f568affe2779e
root <root@jonas-VirtualBox.(none)>
2011-02-18 13:38:30 +0100

    register sucks and lost some files in last commit

--- packages/register/init.php
+++ packages/register/init.php
@@ -32,70 +32,15 @@ class package_register extends package {
 	 
     public function __action_main() {
 		package::addCssFile('register.css', 'register');
+		$fields = array();
+		$additionalFields = userField::getList();
 		return true;
     }
 	public static function  __tpl_showRegisterLink() {
         package::$tpl->display(self::getTplDir('register') . 'register.tpl');
         return true;
     }
- 
- public function __action_register_submit(){
-		$rules=0;
-		$username= mysql_real_escape_string(strtolower($_POST['username']));
-		$email= mysql_real_escape_string($_POST['email']);
-		$password= mysql_real_escape_string($_POST['password']);
-		if (isset($_POST['rules'])) $rules = $_POST['rules'];
+ 	public function __action_register_submit(){
 		
-		if(!$username || !$email || !$password) {
-			
-			throw new lttxError('LN_REGISTER_ERROR_1'); 
-			exit();
-		}
-
-		if (!$rules){
-			throw new lttxError('LN_REGISTER_ERROR_2');
-			exit()	;
-		}
-
-		if (!preg_match ("/^[0-9a-z_-]{3,15}$/i", $username)) {
-			throw new lttxError('LN_REGISTER_ERROR_4');
-			exit();
-		}
-
-		$pos = strpos ($email, "@");
-		if ($pos < 1 ) { 
-			throw new lttxError('LN_REGISTER_ERROR_3');
-			exit();
-		}
-		
-		if(!package::$user){
-	
-		$user = new user(0);
-		
-		$array_data['race']='0';
-		$array_data['userGroup']='0';
-		$date = new Date();
-		$array_data['registerDate'] = $date->getDbTime();
-		$ret=$user->register($username,$password,$email,$array_data);
-		
-		$return_msg='';
-		if(is_a($ret, 'user')){
-			$return_msg=  'ret OK';
-		}else if ($ret==-1){
-			throw new lttxError('LN_REGISTER_ERROR_5');
-		}elseif($ret==-2){
-			throw new lttxError('LN_REGISTER_ERROR_6');
-		}elseif($ret==-3){
-			throw new lttxError('LN_REGISTER_ERROR_7');
-		}else{
-			$return_msg=  'ret ?';
-		}
-		
-			package::debug('new registration:'.$username);
-			$this->_theme = 'register_ok.tpl';
-			return true;
-		
-		}
-			
-	}	
+	}
 }
--- tpl/default/header.tpl
+++ tpl/default/header.tpl
@@ -5,7 +5,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
 <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>{$TITLE}</title>
+<title>{$TITLE}{if isset($PAGE_TITLE)} {$PAGE_TITLE}{/if}</title>
 {foreach from=$CSS_FILES item=CSS_FILE}
 	<link rel="stylesheet" type="text/css" href="{$CSS_FILE}">
 {/foreach}
