f7c3203e82c169137b53d711171f14c076ccf2ec
SnooP <info@sisoware.de>
2011-06-06 15:03:43 +0200

    new Modul Impressum

---
+++ packages/impressum/init.php
@@ -0,0 +1,56 @@
+<?php
+/**
+ * @package impressum
+
+ */
+class package_impressum extends package {
+    /**
+     * Package name
+     * @var string
+     */
+    protected $_packageName = 'impressum';
+
+	private static $_options = false;
+	
+    /**
+     * Default template
+     * @var string
+     */
+    protected $_theme = 'main.tpl';
+
+    /**
+     * Avaibilbe actions in this package
+     * @var array
+     */
+    protected $_availableActions = array('main');
+
+    /**
+     * Main action displays a table in content area
+     */
+    public function __action_main() {
+		if(!self::$_options)
+            self::$_options = new option('Impressum');
+        $this->_initialized = true;
+	
+	
+		
+		$ImpressumMail = self::$_options->get('ImpressumMail');
+		$ImpressumName = self::$_options->get('ImpressumName');
+		$ImpressumStreet = self::$_options->get('ImpressumStreet');
+		$ImpressumCity = self::$_options->get('ImpressumCity');
+		$ImpressumTel = self::$_options->get('ImpressumTel');
+		$ImpressumFax = self::$_options->get('ImpressumFax');
+
+        self::$tpl->assign('ImpressumMail', $ImpressumMail );
+		self::$tpl->assign('ImpressumName', $ImpressumName );
+		self::$tpl->assign('ImpressumStreet', $ImpressumStreet );
+		self::$tpl->assign('ImpressumCity', $ImpressumCity );
+		self::$tpl->assign('ImpressumTel', $ImpressumTel );
+		self::$tpl->assign('ImpressumFax', $ImpressumFax );
+ 
+	
+		self::$tpl->display(self::getTplDir('impressum') . 'main.tpl');
+        return true;
+    }
+	
+}
---
+++ tpl/default/impressum/main.tpl
@@ -0,0 +1,33 @@
+{include file=$HEADER}
+<h2>{#header_0#}</h2><br>
+
+<h3>{#header_1#}</h3>
+{$ImpressumName}<br>
+{$ImpressumStreet}<br>
+{$ImpressumCity}<br>
+
+<h3>{#header_2#}</h3>
+{#header_3#} {$ImpressumTel}<br>
+{#header_4#} {$ImpressumFax}<br>
+{#header_5#} {$ImpressumMail}<br>
+<br>
+<h3>{#header_6#}</h3>
+
+{$ImpressumName}<br>
+{$ImpressumStreet}<br>
+{$ImpressumCity}<br>
+
+<br><br>
+<h3>{#header_7#}<br></h3>
+{#content_1#}
+<br><br>
+<h3>{#header_8#}<br></h3>
+{#content_2#}
+
+
+
+{include file=$FOOTER}
+ 
+
+
+
