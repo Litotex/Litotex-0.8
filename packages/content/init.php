fa38dc7787c6f9e4172d02052b2478408ce2b0d9
Jonas Schwabe <jonas.schwabe@gmail.com>
2012-01-01 22:36:10 +0100

    content module

--- packages/content/init.php
+++ packages/content/init.php
@@ -5,8 +5,10 @@ class package_content extends package{
 	protected $_availableActions = array('main');
 	public function __action_main(){
 		$id = (isset($_GET['ID']))?$_GET['ID']:0;
-		$contentData = self::$db->Execute("SELECT `title`, `text`, `lastEdit`, `editUser` FROM `lttx".package::$dbn."_contents` WHERE `ID` = ?", array($id));
-		if(!isset($contentData->fields[0])){
+		$contentData = self::$db->prepare("SELECT `title`, `text`, `lastEdit`, `editUser` FROM `lttx".package::$dbn."_contents` WHERE `ID` = ?");
+		$contentData->execute(array($id));
+		$contentData = $contentData->fetch();
+		if(!isset($contentData[0])){
 			$error = self::$packages->loadPackage(LITO_ERROR_MODULE, true);
                         if(!$error){
                                 header('HTTP/ 500');
@@ -14,10 +16,10 @@ class package_content extends package{
                         }
                         $error->__action_404();
 		}
-		self::$tpl->assign('PAGE_TITLE', $contentData->fields[0]);
-		self::$tpl->assign('title', $contentData->fields[0]);
-		self::$tpl->assign('text', $contentData->fields[1]);
-		self::$tpl->assign('editUser', new user($contentData->fields[3]));
+		self::$tpl->assign('PAGE_TITLE', $contentData[0]);
+		self::$tpl->assign('title', $contentData[0]);
+		self::$tpl->assign('text', $contentData[1]);
+		self::$tpl->assign('editUser', new user($contentData[3]));
 		return true;
 	}
 }
