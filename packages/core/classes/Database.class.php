78f840548789946d6bbd66a7a9038afb30116e9c
sebix <szebi@gmx.at>
2012-01-27 23:02:18 +0100

    preliminary commit; Database class; refs #8

---
+++ packages/core/classes/Database.class.php
@@ -0,0 +1,117 @@
+<?php
+/*
+ * Copyright (c) 2010 Litotex
+ * 
+ * Permission is hereby granted, free of charge,
+ * to any person obtaining a copy of this software and
+ * associated documentation files (the "Software"),
+ * to deal in the Software without restriction,
+ * including without limitation the rights to use, copy,
+ * modify, merge, publish, distribute, sublicense,
+ * and/or sell copies of the Software, and to permit
+ * persons to whom the Software is furnished to do so,
+ * subject to the following conditions:
+ * 
+ * The above copyright notice and this permission notice
+ * shall be included in all copies or substantial portions
+ * of the Software.
+ * 
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES
+ * OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND
+ * NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT
+ * HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY,
+ * WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+ * OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+ * DEALINGS IN THE SOFTWARE.
+ */
+
+/* This is an extension of the PDO-Class and supports some more methods,
+ * than those which are provided.
+ */
+
+class Database extends PDO {
+	/*
+	 * We'll count all performed Queries
+	 * for profiling issues
+	 */
+	private $queryCount = 0;
+	
+	/*
+	 * Creates an instance of the original PDO-Class and
+	 * sets some attributes
+	 */
+	public function __construct($dsn, 
+								$username=null, 
+								$password=null, 
+								$driver_options=null) {
+		parent::__construct($dsn, $username, $password, $driver_options);
+        try {
+        	// throw exceptions instead of raise warnings
+			$this->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION); 
+			// use our own (extended) Statement Class
+			$this->setAttribute(PDO::ATTR_STATEMENT_CLASS, array('DBStatement', array($this)));
+		} catch (PDOException $e) {
+			throw new LitotexError("Could not set Attributes on PDO: " . $e->getMessage());
+		}
+    }
+    
+    /*
+     * Extending the execution methods for logging and counting
+     */
+	public function exec($sql, $logIt = true) {
+		$sql = str_replace('lttx1_', 'lttx' . Package::$pdbn . '_', $sql);
+		if ($logIt)
+			Logger::debug($sql, LOG_DEBUG);
+		$this->queryCount++;
+		return parent::exec($sql);
+	}
+	public function query($sql) {
+		Logger::debug($sql, LOG_DEBUG);
+		$this->queryCount++;
+		return parent::query($sql);
+	}
+	public function prepare($statement, $options = array()) {
+		Logger::debug('SQL: '.$statement.' Options: ' . var_export($options, true), LOG_DEBUG);
+		return parent::prepare($statement, $options);
+	}
+	
+	/* Get an associative array of results for the query
+	 * This nice code is from here: http://stackoverflow.com/questions/5175357/extending-pdo-class
+	 */
+	public function getAssoc($sql, $params = array()) {
+		try {
+			$stmt = $this->prepare($sql);
+			$params = is_array($params) ? $params : array($params);
+			$stmt->execute($params);
+			return $stmt->fetchAll(PDO::FETCH_ASSOC);
+		} catch (Exception $e) {
+			throw new LitotexException(
+	  			__METHOD__ . ' Exception Raised for this SQL: ' . implode($sql) .
+				' Params: ' . var_export($params, true) .
+				' Error_Info: ' . var_export($this->errorInfo(), true) . $e);
+		}
+	}
+	
+	/*
+	 * @return	int	number of performed queries
+	 */
+	public function getQueryCount() {
+		return $this->queryCount;
+	}
+}
+
+class DBStatement extends PDOStatement {
+	private $pdo;
+	
+	protected function __construct(Database $pdo) {
+		$this->pdo = $pdo;
+	}
+
+	public function execute($params = array()) {
+//		Logger::debug(implode($params), LOG_DEBUG);
+		return parent::execute($params);
+	}
+}
+
+?>
